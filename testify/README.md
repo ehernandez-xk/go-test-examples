d36655a0d006a8909d547214f455ddfbece17bc8
ehernandez <ehernandez@xumak.com>
2017-07-12 07:26:38 -0600

    New table test using testify

--- testify/README.md
+++ testify/README.md
@@ -1,1 +1,6 @@
 Using https://github.com/stretchr/testify
+
+
+Reference in the [blog](https://medium.com/agrea-technogies/basic-testing-patterns-in-go-d8501e360197)
+In the TestAtoi() test
+`go test -v`
--- testify/main_test.go
+++ testify/main_test.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSomething(t *testing.T) {
+func TestFacto(t *testing.T) {
 	t1 := 5 + 4 + 3 + 2 + 1
 	t2 := facto(5)
 	assert.Equal(t, t1, t2, "they should be equal")
 
 	f := facto(-1)
 	assert.Equal(t, 0, f, "factorial of -1 should be 0")
+}
+
+//I'm testing the Atoi(string) function from the package strconv
+func TestAtoi(t *testing.T) {
+	tests := map[string]struct {
+		input  string
+		output int
+		err    error
+	}{
+		"successful conversion": {input: "1", output: 1, err: nil},
+		"invalid integer":       {input: "a text", output: 0, err: &strconv.NumError{}},
+	}
+
+	for testName, test := range tests {
+		t.Logf("Running test case %s", testName)
+
+		output, err := strconv.Atoi(test.input)
+		assert.IsType(t, test.err, err)
+		assert.Equal(t, test.output, output)
+	}
 }
